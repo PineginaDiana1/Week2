8cbc46a4716b746c7d5257c48e416b8ce5c50f4e
dianaPi <logvinovadiana@gmail.com>
2025-05-09 16:02:18 +0300

    Initial commit

---
+++ src/test/java/api/SuccessReg.java
@@ -0,0 +1,17 @@
+package api;
+
+public class SuccessReg {
+    private Integer id;
+    private String token;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public SuccessReg() {
+    }
+}
